34b0f6345f99aac35f3864d911a18d059ae9a243
kunalkudande <kudandekunal1410@gmail.com>
2024-01-22 19:24:54 +0530

    Update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import { useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -8,11 +9,17 @@ import { Todos } from './components/Todos'
 function App() {
   const [todos, setTodos] = useState([]);
 
-  // fetch("http://localhost:3000/todos")
-  //   .then(async function(res){
-  //       const json = await res.json();
-  //       setTodos(json.todos);
-  //   })
+  useEffect(()=>{       //used for fetching the added todo in db to frontend
+    //setInterval(()=> {
+      fetch("http://localhost:3000/todos")
+      .then(async function(res){
+          const json = await res.json();
+          setTodos(json.todos);
+      })
+    }, [Createtodo])
+
+    //}, 5000);
+    
   
 
   return (
